dcb03ffe062ea863af44fe49e1b56b02207f2cd2
Maciko84 <105291597+Maciko84@users.noreply.github.com>
2024-03-07 15:17:20 +0100

    Initial commit.

---
+++ .d.ts
@@ -0,0 +1,83 @@
+/**
+ * WARNING: These are just TYPES declarations!
+ * ///Start(new line)import { Maciko } from "@types/maciko.ts";(new line)//end
+*/
+export const Maciko: {
+    /**
+     * Adds properties to element with given id.
+     * @param id id of element to change.
+     * @param props properties to add to element.
+     */
+    propToId(id: string, props: object): HTMLElement;
+
+    /**
+     * Parses and applies a CSS StyleSheet to page.
+     * @param css CSS Code to parse.
+     */
+    CSS(css: string): void;
+    /**
+     * Adds properties to elements with given class.
+     * @param clas class name of elements to change.
+     * @param props properties to add.
+     */
+    propToClass(clas: string, props: object): HTMLElement[];
+    /**
+     * Adds properties to elements with given tag name.
+     * @param el tag name of elements to change.
+     * @param props properties to add.
+     */
+    propToElement(el: string, props: object): HTMLElement
+    /**
+     * Creates a new DOM element with given properties.
+     * @param tag Tag Name of element.
+     * @param attributes Attributes to add.
+     * @param children Children to add to element (can be a createElement call or string with text)
+     */
+    createElement(tag: string, attributes: object, ...children: (string | HTMLElement)[]): HTMLElement;
+    /**
+     * Setups the website.
+     * @param title Title of the website.
+     * @param elements elements to add to body.
+     */
+    Setup(title: string, ...elements: (HTMLElement | string)[]): void;
+    /**
+     * Collection of functions for creating elements.
+     */
+    elements: {
+        /**
+         * Creates a Img element.
+         * @param url url to image.
+         * @param title title of element (displayed on hover).
+         */
+        Img(url: string, title?: string): HTMLElement;
+        /**
+         * Creates a Link element.
+         * @param url url to link.
+         * @param title title of element (displayed on hover).
+         * @param id id of element.
+        */
+        Link(url: string, title?: string, id?: string): HTMLElement;
+        /**
+         * Creates a line break.
+         */
+        Br(): HTMLElement;
+        /**
+         * Creates a header.
+         * @param level Level of the header (from 1 to 6).
+         * @param text text of the header.
+         */
+        H(level: 1 | 2 | 3 | 4 | 5 | 6 | "1" | "2" | "3" | "4" | "5" | "6", text: string): HTMLElement;
+        /**
+         * Collection of functions for creating media embeds.
+         */
+        media: {
+            /**
+             * Creates an youtube video embed.
+             * @param id Id of the video
+             * @param width width of the video (default: 560)
+             * @param height height of the video (default: 315)
+             */
+            yt(id: string, width?: number, height?: number): HTMLElement;
+        }
+    }
+};
---
+++ errors.js
@@ -0,0 +1,35 @@
+export const Maciko = {
+    createElement(tag, attributes, ...children) {
+    throw "Maciko.TS incorrectly imported"
+    },
+    Setup: (...elements) => {
+        throw "Maciko.TS incorrectly imported"
+    },
+    propToId(id, props) {
+        throw "Maciko.TS incorrectly imported"
+    },
+    propToClass(clas, props) {
+        throw "Maciko.TS incorrectly imported"
+    },
+    propToElement(elem, props) { 
+        throw "Maciko.TS incorrectly imported"
+    },
+    CSS(css) {
+        throw "Maciko.TS incorrectly imported"
+    },
+    elements: {
+        Img(url, title) {
+        throw "Maciko.TS incorrectly imported"
+        },
+        H(level, text) {
+            throw "Maciko.TS incorrectly imported"
+        },
+        Link(url, title) {
+        throw "Maciko.TS incorrectly imported"
+        },
+        Br() { throw "Maciko.TS incorrectly imported" },
+        media: {
+            yt(id, width, height) { throw "Maciko.TS incorrectly imported"}
+        }
+    }
+}
